ffff79e7982594720a15422bcc97e5e80dda5bd0
Milcah <code@milcah.dev>
2021-10-16 19:58:58 -0700

    fixes for real with async await

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,18 @@ import Styles from "../styles/Index.module.scss"
 const Blog = () => {
   const [ prompts, setPrompts ] = useState([])
 
-  const getPrompts = async (data) => {
-    const res = await fetch('api/prompt')
-    const result = await res.json()
-    setPrompts(result)
+  const fetchData = async () => {
+    const response = await fetch('api/prompt');
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const prompts = await response.json();
+    return setPrompts(prompts);
   }
 
-  useEffect(()=> {
-    getPrompts()
+  useEffect(() => {
+    fetchData()
   }, [])
 
   return (
