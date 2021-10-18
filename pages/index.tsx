b78af604d84f68336c28c56c3eb19b02057425ad
Milcah <code@milcah.dev>
2021-10-18 00:05:21 -0700

    returns posts with all prompts

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Post, { PostProps } from "../components/Post"
 const Blog = () => {
   const [ prompts, setPrompts ] = useState([])
 
-  const getPrompts = async (data) => {
+  const getPrompts = async () => {
     const res = await fetch('api/prompt')
     const result = await res.json()
     setPrompts(result)
@@ -22,7 +22,7 @@ const Blog = () => {
         <main>
           {prompts.map((post) => (
             <>
-              <p>{post.id}</p>
+              <p key={post.id}>{post.id}</p>
               <div key={post.id} className="post">
                 <Post post={post} />
               </div>
