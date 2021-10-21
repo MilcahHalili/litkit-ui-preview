3e7af7736492e1822519b426a315bbe85aba5e3a
Milcah <code@milcah.dev>
2021-10-21 16:58:48 -0700

    installs sass, removes deprecated zeit sass

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 const path = require('path')
-const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({
-    cssModules: true
-})
+module.exports = {
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000
+                }
+            }
+        });
+
+        return config;
+    }
+};
 
 module.exports = {
     sassOptions: {
