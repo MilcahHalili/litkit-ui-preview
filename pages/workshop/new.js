a707de1d01605e0304443c728a7ae023f0374748
Milcah <code@milcah.dev>
2021-11-01 18:53:09 -0700

    creates workshop

---
+++ pages/workshop/new.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+
+const NewWorkshop = () => {
+  const [ workshopName, setWorkshopName ] = useState([])
+  const router = useRouter()
+
+  const createWorkshop = async (data) => {
+    data = {
+      name: workshopName
+    }
+    const res = await fetch('/api/workshop/new', {
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    })
+    const result = await res.json()
+    console.log(result)
+  }
+
+  const handleChange = e => {
+    setWorkshopName(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    await e.preventDefault()
+    createWorkshop()
+    console.log('yay!')
+    router.push('/#__next', '/')
+  }
+
+  return (
+    <>
+      <h1>Create new workshop</h1>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="name"
+            onChange={handleChange}
+          />
+          <input
+            type="submit" value="Create"
+          />
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default NewWorkshop
