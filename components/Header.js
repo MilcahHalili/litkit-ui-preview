108e80ab8f95ab8a7aa37918666e06065102f99e
Milcah <code@milcah.dev>
2021-11-03 23:28:24 -0700

    removes typescript

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,9 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Styles from "../styles/Components/Header.module.scss"
 
-const Header: React.FC = () => {
+const Header = () => {
   const router = useRouter();
-  const isActive: (pathname: string) => boolean = (pathname) =>
-    router.pathname === pathname;
+  const isActive = (pathname) => router.pathname === pathname;
 
   let left = (
     <div className={Styles.left}>
