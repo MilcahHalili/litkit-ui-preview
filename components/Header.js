2022765d3d9503e19712990d0b48e6b01b3e0066
Patricia Realini <patriciarealini@github.com>
2021-11-05 16:15:00 -0700

    enable ESLint & turn ooff rules that are currently throwing Warnings. Also turned off rule , TODO: refactor function declarations in /pages/api/prompt/create.js.

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,12 @@ const Header = () => {
     <div className={Styles.left}>
       <Link href="/">
         <a className={Styles.boldA} data-active={isActive("/")}>
-          <img src="https://i.imgur.com/NxwZ9x0.png" width="100px" height="100px"/>
+            <img
+                src="https://i.imgur.com/NxwZ9x0.png"
+                alt=""
+                width="100px"
+                height="100px"
+            />
         </a>
       </Link>
     </div>
--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import Header from "./Header";
 import Styles from "../styles/Components/Layout.module.scss"
 
