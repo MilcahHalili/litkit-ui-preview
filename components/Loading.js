1c806901788a3f23a97d248846eea85e009b4044
Patricia Realini <patriciarealini@github.com>
2021-11-12 03:32:43 -0800

    placeholder Loading icon for demo *

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAsterisk } from '@fortawesome/free-solid-svg-icons'
+
+import Styles from "../styles/Components/Loading.module.scss"
+
 export default function Loading() {
   return (
-    <div className='container'>
-      <p>Loading...</p>
+    <div className={Styles.loading}>
+      <FontAwesomeIcon icon={faAsterisk} className={Styles.loadingIcon} size="5x" spin/>
     </div>
   );
 }
